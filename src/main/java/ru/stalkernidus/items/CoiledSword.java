e284c780e5b4dbccc812655fe6ab5228704c1d43
StalkerZero <iron-3@mail.ru>
2022-04-13 14:51:05 +0300

    Добавил текстуру костра

--- src/main/java/ru/stalkernidus/items/CoiledSword.java
+++ src/main/java/ru/stalkernidus/items/CoiledSword.java
@@ -1,9 +1,19 @@
 package ru.stalkernidus.items;
 
+/*import net.minecraft.world.effect.AttackDamageMobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;*/
 import net.minecraft.world.item.*;
 
 public class CoiledSword extends SwordItem {
     public CoiledSword() {
         super(Tiers.NETHERITE, 5, -2.8F, (new Item.Properties()).tab(CreativeModeTab.TAB_COMBAT).fireResistant());
     }
+
+   /* @Override
+    public boolean hurtEnemy(ItemStack itemStack, LivingEntity target, LivingEntity attacker) {
+        target.addEffect(new MobEffectInstance(MobEffects.))
+        return super.hurtEnemy(itemStack, target, attacker);
+    }*/
 }
